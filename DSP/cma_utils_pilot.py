5705089c3bdc13186e893702c78f99fba7acb9ac
yasho srikar <medhabg@gmail.com>
2026-04-20 00:03:10 +0530

    generating frames with pilots

---
+++ DSP/cma_utils_pilot.py
@@ -0,0 +1,123 @@
+import numpy as np
+
+def gen_I_Q_qpsk(N_symbols):
+    levels = np.array([-1, 1]) / np.sqrt(2)
+
+    qam_symbols_x = (np.random.choice(levels, N_symbols) +
+                     1j * np.random.choice(levels, N_symbols))
+    qam_symbols_y = (np.random.choice(levels, N_symbols) +
+                     1j * np.random.choice(levels, N_symbols))
+
+    E_in = np.column_stack((qam_symbols_x, qam_symbols_y))
+    return E_in
+
+def first_eleven():
+    X = np.array([
+        -3+3j,  3+3j, -3+3j,  3+3j, -3-3j, 3+3j, -3-3j, -3-3j,  3+3j,  3-3j,  3-3j
+        ])
+    Y = np.array([
+        -3-3j, -3-3j,  3-3j, -3+3j, -3+3j, 3+3j, -3-3j, -3+3j,  3-3j,  3+3j,  3-3j
+        ])
+    norm = np.sqrt(18)
+    return X / norm, Y / norm
+
+def lfsr_sequence(seed, length):
+    # 10-bit shift register based on x^10 + x^8 + x^4 + x^3 + 1
+    
+    state = seed & 0x3FF  # keep 10 bits
+    seq = []
+
+    for _ in range(length):
+        b9 = (state >> 9) & 1 #x^10
+        b7 = (state >> 7) & 1 #x^8
+        b3 = (state >> 3) & 1 #x^4
+        b2 = (state >> 2) & 1 #x^3
+        b0 = (state >> 0) & 1 #1
+
+        new_bit = b9 ^ b7 ^ b3 ^ b2 ^ b0
+
+        # output is last bit
+        seq.append(b0)
+
+        # state update with feedback loop
+        state = ((state << 1) & 0x3FF) | new_bit
+
+    return np.array(seq)
+
+
+def generate_lfsr_pilots(seed, num_pilots):
+    bits = lfsr_sequence(seed, num_pilots * 2)
+    symbols = []
+    # generating symbols from the bits
+    # using pairwise bits to get the real and imag part of symbol
+    for i in range(num_pilots):
+        b1 = bits[2*i]
+        b2 = bits[2*i + 1]
+        # generate real part as 3 if odd bit is 1, else -3
+        # generate imag part as 3 if even bit is 1, else -3
+        real = 1 if b1 else -1
+        imag = 1 if b2 else -1
+
+        symbols.append(real + 1j * imag)
+
+    return np.array(symbols) / np.sqrt(2)
+
+
+def generate_frame():
+    frame_len = 3712
+
+    frame_x = np.zeros(frame_len, dtype=complex)
+    frame_y = np.zeros(frame_len, dtype=complex)
+
+    # first 32 bit section:
+
+    x_pilots_11, y_pilots_11 = first_eleven()
+
+    idx = 0
+    frame_x[idx:idx+11] = x_pilots_11
+    frame_y[idx:idx+11] = y_pilots_11
+    idx += 11
+    
+    data = gen_I_Q_qpsk(21)
+    frame_x[idx : idx+21] = data[ : , 0]
+    frame_y[idx : idx+21] = data[ : , 1]
+    idx += 21
+
+    # remaining sections:
+
+    # seed X = 0x19E, seed Y = 0x0D0 for the remaining 115 pilots
+    lfsr_x = generate_lfsr_pilots(0x19E, 115)
+    lfsr_y = generate_lfsr_pilots(0x0D0, 115)
+
+    print(lfsr_x[:10])
+
+    for i in range(115):
+        # pilot
+        frame_x[idx] = lfsr_x[i]
+        frame_y[idx] = lfsr_y[i]
+        idx += 1
+
+        # data
+        data = gen_I_Q_qpsk(31)
+        frame_x[idx:idx+31] = data[:, 0]
+        frame_y[idx:idx+31] = data[:, 1]
+        idx += 31
+
+    return np.column_stack((frame_x, frame_y))
+
+
+def generate_stream(N_total):
+    frame_len = 3712
+    num_frames = int(np.ceil(N_total / frame_len)) + 1
+
+    stream = np.vstack([generate_frame() for _ in range(num_frames)])
+
+    # making bitstream start from somewhere random
+    offset = np.random.randint(1, frame_len)
+    stream = stream[offset:]
+
+    return stream[:N_total]
+
+N_symbols = 10000
+E_in = generate_stream(N_symbols)
+
